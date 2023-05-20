1e50dba7fa8bb67608c5f4b4ba43b6282b0b35a8
Alberto Miranda <alberto.miranda@sou.inteli.edu.br>
2023-05-20 19:15:18 -0300

    Frontend first commit

---
+++ src/frontend/front/pages/bets.jsx
@@ -0,0 +1,9 @@
+//import global css
+import "../styles/globals.css";
+export default function Bets() {
+  return (
+    <div>
+      <h1 className="text-blue-600">Bets</h1>
+    </div>
+  );
+}
---
+++ src/frontend/safe_mcqueen/src/components/componente.jsx
@@ -0,0 +1,9 @@
+export default function Teste2({ name, idade, fact }) {
+  return (
+    <div>
+      <h1>Nome: {name}</h1>
+      <p className="text-blue-600 ">idade:{idade}</p>
+      <p className="text-blue-600">fato :{fact}</p>
+    </div>
+  );
+}
