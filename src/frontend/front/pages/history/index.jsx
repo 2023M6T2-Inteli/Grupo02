677bab19bdd838865b39a01362e6e688ec6a5ea2
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2023-05-22 15:25:32 -0300

    Front_Add_Header-tag

---
+++ src/frontend/front/pages/history/index.jsx
@@ -0,0 +1,10 @@
+import Header from '../../components/Header'
+
+export default function History() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-blue-600">sussa</h1>
+    </div>
+  );
+}
