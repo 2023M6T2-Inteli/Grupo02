f7b97cefde620bddbfcfd4f80b43954194a39e81
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2023-05-23 16:02:23 -0300

    SP3-history_and_Header

---
+++ src/frontend/front/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(params) {
+    return (
+        <>
+        <div className="Header bg-azul">
+            <div><img class="w-3/12" src="https://i.ibb.co/Fg0ZfTS/logo.png" /></div>
+            <div className="display">
+                <div><a href="../history"><img className='icons' src="https://i.ibb.co/QFCHHs6/folder-open-1.png" alt="" /></a></div>
+                <div><a href="../"><img className='icons' src="https://i.ibb.co/HhShB6N/house-chimney-1.png" alt="" /></a></div>
+            </div>
+        </div>
+        </>
+       
+    );
+}
