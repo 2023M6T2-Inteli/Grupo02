4bcdbd7c22cba9910a0a532567e243e821d6fd80
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-05-24 15:44:59 -0300

    Atualizações tela de setup

--- src/frontend/front/components/Search.jsx
+++ src/frontend/front/components/Search.jsx
@@ -1,9 +1,14 @@
-export default function Search() {
+import { Search } from "react-bootstrap-icons";
+
+export default function Searchh() {
     return(
         <>
         <div>
-            <div>
-                <input className="bg-gray-50 w-64 text-sm py-2 pl-2 "></input>
+            <div className="bg-gray-50 w-80 py-2 pl-2 rounded-full flex justify-between">
+                <input className="text-sm w-64" placeholder="Pesquise por ID do espaço confinado" />
+                <button className="w-8">    
+                    <Search />
+                </button>
             </div>
             {/* <div className="justify-between group flex items-center bg-blue-200 max-w-xs rounded-full">
                 <input className="w-64 focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm border-none bg-transparent" type="text" aria-label="Filter projects" placeholder="Pesquise por ID do espaço confinado"></input>
--- src/frontend/front/pages/setup/index.jsx
+++ src/frontend/front/pages/setup/index.jsx
@@ -1,5 +1,5 @@
-import Registration from "@/components/Registro";
-import Search from "@/components/Search";
+import Registration from "@/components/Registration";
+import Searchh from "@/components/Search";
 import Header from "@/components/Header";
 
 export default function Inspection(){
@@ -8,20 +8,22 @@ export default function Inspection(){
             <div>
                 <Header />
             </div>
-            <div className="mt-16 ml-16 pt-10 pl-10 w-1/2 h-96 bg-azul rounded-lg">
-                <div className="font-inter text-white">
-                    <p>Selecione o espaço confinado que deseja inspecionar.</p>
+            <div style={{display: 'flex', width: 'calc(100vw - 64px)', margin: '0 32px'}}>
+                <div className="mt-16 pt-10 pl-10 w-1/2 h-96 bg-azul rounded-lg">
+                    <div className="font-inter text-white">
+                        <p>Selecione o espaço confinado que deseja inspecionar.</p>
+                    </div>
+                    <div className="mt-4">
+                        <Searchh />
+                    </div>
+                    <div className="mt-6">
+                        <Registration />
+                    </div>
                 </div>
-                <div className="mt-4">
-                    <Search />
-                </div>
-                <div className="mt-6">
-                    <Registration />
-                </div>
-            </div>
-            <div className="ml-16 mt-16 w-1/2 h-64 bg-azul rounded-lg">
-                <div className="font-inter text-white pt-10 pl-10">
-                    <p>Selecione um espaço confinado para visualizar uma prévia de sua rota.</p>
+                <div className="ml-16 mt-16 w-1/2 h-64 bg-azul rounded-lg">
+                    <div className="font-inter text-white pt-10 pl-10">
+                        <p>Selecione um espaço confinado para visualizar uma prévia de sua rota.</p>
+                    </div>
                 </div>
             </div>
             <div>
