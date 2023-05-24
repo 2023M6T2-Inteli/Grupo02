96ec9903fe8721a2024130d279e47ee5160cc4d3
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-05-23 21:42:25 -0300

    Criação da tela de setup

---
+++ src/frontend/front/components/Registration.jsx
@@ -0,0 +1,21 @@
+export default function Registration(inspecao) {
+    return(
+        <>
+        <div>
+            <div>
+                <span>
+                    
+                </span>
+            </div>
+            <div className="icons">
+                <div className="pencil">
+                    <i class="fi fi-sr-pencil"></i>
+                </div>
+                <div className="next">
+                    <i class="fi fi-ts-angle-double-small-right"></i>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
