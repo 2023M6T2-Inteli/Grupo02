81371d5649d36faaff9ddb88c967838b3269d11b
Lukovsk <lucas.jupiter23@gmail.com>
2023-05-24 17:05:37 -0300

    add: mais refatoração do código e algumas mudanças em lidar.py para a margem de segurança

--- src/gazebo/lidar.py
+++ src/gazebo/lidar.py
@@ -4,7 +4,8 @@ import numpy as np
 class Lidar():
     def __init__(self, node):
         self.ranges_ = []
-
+        self.permission = True
+        
         self.subscription_ = node.create_subscription(
             msg_type=LaserScan,
             topic='/scan',
@@ -12,19 +13,27 @@ class Lidar():
             qos_profile=10
         )
         
+    # Recebe o array de distâncias do Lidar
     def lidar_callback(self, msg):
         self.ranges_ = msg.ranges
-        print("Ranges: ", self.ranges_)
+        # print("Ranges: ", self.ranges_)
         
-    def margem_de_segurança(self):
-        frente_primeiro_quadrante = np.array(self.ranges_[0:8]) 
-        frente_segundo_quadrante = np.array(self.ranges_[352:359])
-        return np.concatenate([frente_primeiro_quadrante, frente_segundo_quadrante])
+    # Cria a margem de segurança
+    def safety_margin(self):
+        front_first_quadrant = np.array(self.ranges_[0:8]) 
+        front_second_quadrant = np.array(self.ranges_[352:359])
+        return np.concatenate([front_first_quadrant, front_second_quadrant])
     
-    def margem_segura(self):
-        print("Margem de segurança: ", self.margem_de_segurança())
-        print("Margem segura", len(set(self.margem_de_segurança())))
-        if len(set(self.margem_de_segurança())) == 1:
-            return True
-        else:
-            return False
+    # Faz o Lidar parar de funcionar
+    def kill_lidar(self):
+        self.permission = False
+    
+    # Verifica se o turtlebot encontrou algum objeto na margem de segurança
+    def check_safety_margin(self):
+        # print("Margem de segurança: ", self.margem_de_segurança())
+        # print("Margem segura", len(set(self.margem_de_segurança())))
+        # Pra saber se tem algo na margem de segurança, precisamos verificar se há algum número dentro do array de segurança menor do que 30 cm (0.3)
+        for i in self.safety_margin():
+            if i > 0.3 and self.permission:
+                return False
+        return False
