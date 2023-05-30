a432ca86627a641ad08d142501cd0ca6288261e6
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-05-30 17:29:54 -0300

    Alterações na tela de setup

--- src/frontend/front/components/Search.jsx
+++ src/frontend/front/components/Search.jsx
@@ -1,11 +1,11 @@
 import { Search } from "react-bootstrap-icons";
 
-export default function Searchh() {
+export default function SearchBar() {
     return(
         <>
         <div>
-            <div className="bg-gray-50 w-5/6 py-2 pl-2 rounded-full flex justify-between">
-                <input className="text-sm font-inter w-2/3 pl-4" placeholder="Pesquise por ID do espaço confinado" />
+            <div className="bg-gray-50 w-5/6 py-2 pl-2 rounded-full flex justify-between text-primary">
+                <input className="text-sm font-inter w-5/6 pl-4" placeholder="Pesquise por ID do espaço confinado" />
                 <button className="w-8">    
                     <Search />
                 </button>
