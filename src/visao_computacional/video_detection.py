cc7a77efd9ac36585f2aa6c2c63642f2414d159f
Alberto Miranda <alberto.miranda@sou.inteli.edu.br>
2023-05-30 17:25:56 -0300

    frame rate defined

--- src/visao_computacional/video_detection.py
+++ src/visao_computacional/video_detection.py
@@ -1,24 +1,24 @@
 # import the opencv library
 import cv2
 from ultralytics import YOLO
+import time
 model = YOLO("./model_filtered.pt")
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
 saved_images = []
 while (True):
-
+    time.sleep(1/30)
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
 
-    results = model.predict(frame, conf=0.7)
- 
+    results = model.predict(frame, conf=0.6)
+
     # Display the resulting frame
-  
 
     if len(results[0]) > 0:
-        
+
         saved_images.append(results[0].plot())
 
     image = results[0].plot()
@@ -28,8 +28,7 @@ while (True):
     # quitting button you may use any
     # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
-       break
-           
+        break
 
 
 # After the loop release the cap object
@@ -43,5 +42,5 @@ for i in range(len(saved_images)):
     cv2.imshow(f'frame {i} de len', saved_images[i])
     if cv2.waitKey(0) & 0xFF == ord('a'):
         break
-    
+
     cv2.destroyAllWindows()
