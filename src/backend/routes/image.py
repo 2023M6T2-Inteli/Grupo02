372d2f65e84007a26551d8930711bc2ddffb166f
Alberto Miranda <alberto.miranda@sou.inteli.edu.br>
2023-06-06 15:53:30 -0300

    supabase routes finished begining to integrate

--- src/backend/routes/image.py
+++ src/backend/routes/image.py
@@ -6,20 +6,33 @@ from dotenv import load_dotenv
 load_dotenv()
 url = os.getenv("url")
 key = os.getenv("api_key")
-router =APIRouter(prefix="/images")
+router = APIRouter(prefix="/images")
 
 
-supabase:Client = create_client(supabase_url="",supabase_key="")
+supabase: Client = create_client(supabase_url="https://xbjclntldtajiazipkem.supabase.co",
+                                 supabase_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhiamNsbnRsZHRhamlhemlwa2VtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NTcyOTIzNSwiZXhwIjoyMDAxMzA1MjM1fQ.LR3l7jL_i3WR4NsU6350WetqdIQSOBf8RZdTG7KzJM0")
 
 
 @router.get("/get/{file}")
 async def get_all(file):
-   
-    return supabase.storage.from_("/Images").get_public_url(f"{file}")
-# @router.post("/add")
-# async def store_image(image:str,name:str):
-#     try:
-#         res =supabase.storage.from_("Images").upload(f"{name}.txt",image)
-#         return str(res)
-#     except Exception as e:
-#         return e
+
+    return supabase.storage.from_("/test").get_public_url(f"{file}")
+
+
+@router.post("/add")
+async def store_image(name: dict):
+    text = name["text"]
+    name = name["name"]
+    try:
+        f = open(f"{name}.txt", "w")
+        f.write(text)
+        f.close()
+
+        with open(f"{name}.txt", "rb+") as f:
+            file = f.read()
+            f.close()
+        res = supabase.storage.from_("/test").upload(f"{name}.txt", file)
+        os.remove(f"{name}.txt")
+        return name
+    except Exception as e:
+        return str(e)
