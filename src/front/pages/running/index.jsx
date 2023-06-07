440d074569cfe8e5bfcaa73f651bc4dc8ff94dd0
taiinteli <99188288+taiinteli@users.noreply.github.com>
2023-06-06 22:43:13 -0300

    updat_run

--- src/front/pages/running/index.jsx
+++ src/front/pages/running/index.jsx
@@ -5,33 +5,34 @@ export default function Running() {
         <div>
             <div className="bg-cinza w-screen h-screen ">
                 <Header />
-                <div className="flex justify-center items-center flex-col m-4 gap-y-6 text-white">
+                <div className="flex justify-center items-center flex-col m-4 gap-y-8 text-white">
                     <div className="flex gap-x-6 flex-wrap">
-                        <div className="bg-azul rounded-xl p-4">
-                            <p>Confira abaixo a trajetória feita pelo robô para realizar a inspeção do espaço selecionado.</p>
-                            <img src="" alt="" />
+                        <div className="bg-azul rounded-xl p-4 flex gap-y-2 flex-col">
+                            <p className="p-4">Confira abaixo a trajetória feita pelo robô para realizar a inspeção do espaço selecionado.</p>
+                            <img src="https://raw.githubusercontent.com/2023M6T2-Inteli/Safe-McQueen/Front-end/src/front/public/Rectangle%2013.png"/>
                         </div>
-                        <div className="flex gap-y-6 flex-col">
-                            <div className="bg-azul rounded-xl p-4 flex justify-center">
+
+                        <div className="md-50 flex gap-y-6 flex-col">
+                            <div className="bg-azul rounded-xl p-4 flex justify-center ">
                                 <p>STATUS</p>
                             </div>
-                            <div className="bg-azul flex rounded-xl p-4">
+                            <div className="bg-azul flex rounded-xl p-4 justify-center gap-x-4">
                                 <div className="p-2 flex items-center flex-col">
                                     <img src="https://raw.githubusercontent.com/2023M6T2-Inteli/Safe-McQueen/Front-end/src/front/public/o2%20(1)%201.png"/>
                                     <p>22%</p>
-
                                 </div>
                                 <div className="p-2 flex items-center flex-col gap-y-3.5">
                                     <img src="https://raw.githubusercontent.com/2023M6T2-Inteli/Safe-McQueen/Front-end/src/front/public/thermometer%201.png"/>
                                     <p>22%</p>
-
                                 </div>
                             </div>
+                            <div className="">
+                                 <div className="bg-azul p-4 rounded-xl h-80 "><p>Logs importantes</p></div>
+                            </div> 
                         </div>
+
                     </div>
-                    <div className="">
-                        <div className="bg-azul p-4 rounded-xl "><p>Logs importantes</p></div>
-                    </div>
+
                 </div>
             </div>
         </div>
